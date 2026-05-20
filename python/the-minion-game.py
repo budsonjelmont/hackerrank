9eee0fda95a12e84a4dbf15a74c5d3166b028c6a
budsonjelmont <budsonjelmont@gmail.com>
2026-05-20 07:37:20 -0400

    Solution for minion game

--- python/the-minion-game.py
+++ python/the-minion-game.py
@@ -1,104 +1,21 @@
 #https://www.hackerrank.com/challenges/the-minion-game/problem?isFullScreen=true
 
-# Algorithmically correct(?) but slow
 def minion_game(string):
-    motifs = {}
-    scores = {'Kevin':0, 'Stuart':0}
+    scores = {"Kevin":0, "Stuart":0}
+    seeds = {"Kevin":0, "Stuart":0}
     for i in range(0,len(string)):
-        for j in range(i+1,len(string)+1):
-            substr = string[i:j]
-            try:
-                motifs[substr] += 1
-            except KeyError:
-                motifs[substr] = 1
-    for k,v in motifs.items():
-        player = 'Kevin' if k[0].lower() in ['a','e','i','o','u'] else 'Stuart'
-        scores[player] += v
+        for p in scores.keys():
+            scores[p] += seeds[p]
+        # Determine which player gets a seed for new substrings starting with this letter
+        player = 'Kevin' if string[i].lower() in ['a','e','i','o','u'] else 'Stuart'
+        seeds[player] += 1
+        scores[player] += 1
     if scores['Kevin'] > scores['Stuart']:
         print(f'Kevin {scores["Kevin"]}')
     elif scores['Kevin'] < scores['Stuart']:
         print(f'Stuart {scores["Stuart"]}')
     else:
         print('Draw')
-
-# Optimized
-'''
-CAGG
-
-None
-/|\
-C A G
-| | |
-A G G
-
-graph = {
-    None:{'C':[0],'A':[1],'G':[2,3]},
-    'C':{'A':[1]},
-    'A':{'G':[2]},
-    'G':{'G':[3]}
-}
-
-def iter_motifs(graph, seq, char, pos, motifs):
-  for nextchar in graph[char]:
-    for nextpos in graph[char][nextchar]:
-        if char is None or nextpos == pos + 1
-            try:
-                motifs[seq + nextchar] += 1
-            except KeyError:
-                motifs[seq + nextchar] = 1
-            iter_motifs(graph, seq + nextchar, next_char, pos+1, motifs)
-  return motifs
-
-# Call it with
-iter_motifs(graph, '', None, -1, {})
-
-*/
-'''
-
-def iter_motifs(graph, seq, char, pos, motifs):
-  for nextchars in graph[char]:
-    for nextpos in nextpos:
-        if char is None or nextpos == pos + 1
-            try:
-                motifs[seq + nextchar] += 1
-            except KeyError:
-                motifs[seq + nextchar] = 1
-            iter_motifs(graph, seq + nextchar, next_char, pos+1, motifs)
-        if char is None:
-            return motifs
-
-substrs = {}
-def iter_substrs(s, subs, pos):
-    if subs != ''
-        try:
-            substrs[subs] += 1
-        except KeyError:
-            substrs[subs] = 1
-    if pos < len(s):
-        iter_substrs(s, subs + s[pos], pos+1)
-        if pos>0:
-            iter_substrs(s, s[pos], pos+1)
-
-iter_substr('CAGG','',0)
-iter_substr('CAGG','C',1)
-iter_substr('CAGG','CA',2)
-
-def minion_game(string):
-    graph = {}
-    motifs = {}
-    for i in range(0,len(string)):
-        charat = string[i]
-        try:
-            motifs[None][charat].append(i)
-        except KeyError:
-            motifs[None][charat] = [i]
-        if i > 0:
-            charprev = string[i-1]
-            try:
-                motifs[charprev].append(i)
-            except KeyError:
-                motifs[(lastchar,curchar)] = [i]
-
 
 if __name__ == '__main__':
     s = input()
